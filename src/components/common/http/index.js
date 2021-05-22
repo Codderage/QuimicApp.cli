4e988c1e7e0c5a60bf6995adb3202bb3c237273e
Sherab Pereira <sherabpereira@gmail.com>
2021-05-22 12:52:16 +0200

    Fix dependency

--- src/components/common/http/index.js
+++ src/components/common/http/index.js
@@ -2,9 +2,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
 
-     "http://localhost/M14/Proyecto-final/quimica/AppQuimica/server/public/api/",
-    // "http://localhost/Clase/ProyectoFinal/Quimica/quimica/AppQuimica/server/public/api/",
-    //"http://localhost/M7_M14/projecte_M14/quimica/AppQuimica/server/public/api/",
+    "http://localhost/M14/Proyecto-final/quimicapp.serv/public/api/",
+  // "http://localhost/Clase/ProyectoFinal/Quimica/quimica/AppQuimica/server/public/api/",
+  //"http://localhost/M7_M14/projecte_M14/quimica/AppQuimica/server/public/api/",
 
 });
 export default instance;
