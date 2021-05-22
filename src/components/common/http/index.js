73a5830eb21ca1b8e5f6b0a8b326619a49282915
Sherab Pereira <sherabpereira@gmail.com>
2021-05-22 13:43:10 +0200

    Cambios parametros

--- src/components/common/http/index.js
+++ src/components/common/http/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
 
-    "http://localhost/M14/Proyecto-final/quimicapp.serv/public/api/",
-  // "http://localhost/Clase/ProyectoFinal/Quimica/quimica/AppQuimica/server/public/api/",
+    "https://quimicback.herokuapp.com/public/api/",
+  //"http://localhost/Clase/ProyectoFinal/Quimica/quimica/AppQuimica/server/public/api/",
   //"http://localhost/M7_M14/projecte_M14/quimica/AppQuimica/server/public/api/",
 
 });
