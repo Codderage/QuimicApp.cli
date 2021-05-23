dcccd3d525a4236af29130a955c5ac50d37f5866
jordi <jordi@jordi.com>
2021-05-23 05:14:23 +0200

    c

--- src/components/practices/Practices.jsx
+++ src/components/practices/Practices.jsx
@@ -194,8 +194,8 @@
 //==========================================================================================================
 //==========================================================================================================
 //==========================================================================================================
-//==========================================================================================================import React, { useEffect, useState } from "react";
-
+//==========================================================================================================
+import React, { useEffect, useState } from "react";
 import axios from "../common/http";
 import { Table, Space } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -270,141 +270,141 @@ const onCreateBut = () => {
   }
 };
 
-const onUpNull = (variable) => {
-  if (variable == null) {
-    return "";
-  } else {
-    return variable;
-  }
-};
+// const onUpNull = (variable) => {
+//   if (variable == null) {
+//     return "";
+//   } else {
+//     return variable;
+//   }
+// };
 
-const onUpdate = async (
-  key,
-  idCompuesto,
-  nombreCompuesto,
-  descripcion,
-  formula,
-  tipo,
-  estructura,
-  e
-) => {
-  //e.preventDefault();
-  //const data = this.state.data.filter(item => item.key !== key);
-  //this.setState({ data, isPageTween: false });
-  //console.log(key, e);
-  //history.push("/editUsuario");
-  // const grupos = await groups(rol, id_grupo, grupo);
-  //console.log(grupos, "AAAAAAAAAAAAAAAAAAAAAA");
-  nombreCompuesto = onUpNull(nombreCompuesto);
-  descripcion = onUpNull(descripcion);
-  formula = onUpNull(formula);
-  tipo = onUpNull(tipo);
-  estructura = onUpNull(estructura);
-  Swal.fire({
-    title: "Editar",
-    html: `<label for='EnombreCompuesto'>Nombre:</label><br>
-    <input class="swal2-input" id='EnombreCompuesto' type='text' value='${nombreCompuesto}'><br>
-    <label for='Edescripcion'>Descripción:</label><br>
-    <input class="swal2-input" id='Edescripcion' type='text' value='${descripcion}'><br>
-    <label for='Eformula'>Fórmula:</label><br>
-    <input class="swal2-input" id='Eformula' type='text' value='${formula}'><br>
-    <label for='Etipo'>Tipo:</label><br>
-    <input class="swal2-input" id='Etipo' type='text' value='${tipo}'><br>
-    <label for='Eestructura'>Estructura:</label><br>
-    <input class="swal2-input" id='Eestructura' type='text' value='${estructura}'>`,
-    // <input id='Eprofe' type='checkbox'>
-    // <label class="swal2-input" for='Eprofe'>&nbsp;Es profesor</label><br>
-    // <input id='Eadmin' type='checkbox'>
-    // <label class="swal2-input" for='Eadmin'>&nbsp;Es admin</label>
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Editar",
-    cancelButtonText: "Cancelar",
-    focusConfirm: false,
-    preConfirm: () => {
-      const EnombreCompuesto =
-        Swal.getPopup().querySelector("#EnombreCompuesto").value;
-      const Edescripcion = Swal.getPopup().querySelector("#Edescripcion").value;
-      const Eformula = Swal.getPopup().querySelector("#Eformula").value;
-      const Etipo = Swal.getPopup().querySelector("#Etipo").value;
-      const Eestructura = Swal.getPopup().querySelector("#Eestructura").value;
-      //alert(Egrupo);
-      //const Eprofe = Swal.getPopup().querySelector("#Eprofe").checked;
-      //const Eadmin = Swal.getPopup().querySelector("#Eadmin").checked;
-      if (!EnombreCompuesto || !Eformula) {
-        Swal.showValidationMessage(`Algún campo obligatorio vacío`);
-      }
-      return {
-        EnombreCompuesto: EnombreCompuesto,
-        Edescripcion: Edescripcion,
-        Eformula: Eformula,
-        Etipo: Etipo,
-        Eestructura: Eestructura,
-      };
-    },
-  }).then((result) => {
-    if (result.isConfirmed) {
-      swal({
-        //title: "Comprobando ...",
-        icon: carga,
-        button: false,
-        allowOutsideClick: false,
-      });
-      let peticion = [];
-      peticion = {
-        nombre: `${result.value.EnombreCompuesto}`,
-        formula: `${result.value.Eformula}`,
-        descripcion: `${result.value.Edescripcion}`,
-        tipo: `${result.value.Etipo}`,
-        estructura: `${result.value.Eestructura}`,
-      };
-      axios
-        .put("compuesto-quimico/" + `${idCompuesto}`, peticion)
-        .then((response) => {
-          //console.log(response.data);
-          if (response.status >= 200 && response.status <= 205) {
-            //console.log(response.data[1].nombre);
-            //console.log(response.data);
-            window.location.reload(true);
-            //Users.array1[key].nombreUsuario = response.data.nombreUsuario;
-            //Users.array1[key].nombre = response.data.nombre;
-            //Users.array1[key].apellidos = response.data.apellidos;
+// const onUpdate = async (
+//   key,
+//   idCompuesto,
+//   nombreCompuesto,
+//   descripcion,
+//   formula,
+//   tipo,
+//   estructura,
+//   e
+// ) => {
+//   //e.preventDefault();
+//   //const data = this.state.data.filter(item => item.key !== key);
+//   //this.setState({ data, isPageTween: false });
+//   //console.log(key, e);
+//   //history.push("/editUsuario");
+//   // const grupos = await groups(rol, id_grupo, grupo);
+//   //console.log(grupos, "AAAAAAAAAAAAAAAAAAAAAA");
+//   nombreCompuesto = onUpNull(nombreCompuesto);
+//   descripcion = onUpNull(descripcion);
+//   formula = onUpNull(formula);
+//   tipo = onUpNull(tipo);
+//   estructura = onUpNull(estructura);
+//   Swal.fire({
+//     title: "Editar",
+//     html: `<label for='EnombreCompuesto'>Nombre:</label><br>
+//     <input class="swal2-input" id='EnombreCompuesto' type='text' value='${nombreCompuesto}'><br>
+//     <label for='Edescripcion'>Descripción:</label><br>
+//     <input class="swal2-input" id='Edescripcion' type='text' value='${descripcion}'><br>
+//     <label for='Eformula'>Fórmula:</label><br>
+//     <input class="swal2-input" id='Eformula' type='text' value='${formula}'><br>
+//     <label for='Etipo'>Tipo:</label><br>
+//     <input class="swal2-input" id='Etipo' type='text' value='${tipo}'><br>
+//     <label for='Eestructura'>Estructura:</label><br>
+//     <input class="swal2-input" id='Eestructura' type='text' value='${estructura}'>`,
+//     // <input id='Eprofe' type='checkbox'>
+//     // <label class="swal2-input" for='Eprofe'>&nbsp;Es profesor</label><br>
+//     // <input id='Eadmin' type='checkbox'>
+//     // <label class="swal2-input" for='Eadmin'>&nbsp;Es admin</label>
+//     showCancelButton: true,
+//     confirmButtonColor: "#3085d6",
+//     cancelButtonColor: "#d33",
+//     confirmButtonText: "Editar",
+//     cancelButtonText: "Cancelar",
+//     focusConfirm: false,
+//     preConfirm: () => {
+//       const EnombreCompuesto =
+//         Swal.getPopup().querySelector("#EnombreCompuesto").value;
+//       const Edescripcion = Swal.getPopup().querySelector("#Edescripcion").value;
+//       const Eformula = Swal.getPopup().querySelector("#Eformula").value;
+//       const Etipo = Swal.getPopup().querySelector("#Etipo").value;
+//       const Eestructura = Swal.getPopup().querySelector("#Eestructura").value;
+//       //alert(Egrupo);
+//       //const Eprofe = Swal.getPopup().querySelector("#Eprofe").checked;
+//       //const Eadmin = Swal.getPopup().querySelector("#Eadmin").checked;
+//       if (!EnombreCompuesto || !Eformula) {
+//         Swal.showValidationMessage(`Algún campo obligatorio vacío`);
+//       }
+//       return {
+//         EnombreCompuesto: EnombreCompuesto,
+//         Edescripcion: Edescripcion,
+//         Eformula: Eformula,
+//         Etipo: Etipo,
+//         Eestructura: Eestructura,
+//       };
+//     },
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       swal({
+//         //title: "Comprobando ...",
+//         icon: carga,
+//         button: false,
+//         allowOutsideClick: false,
+//       });
+//       let peticion = [];
+//       peticion = {
+//         nombre: `${result.value.EnombreCompuesto}`,
+//         formula: `${result.value.Eformula}`,
+//         descripcion: `${result.value.Edescripcion}`,
+//         tipo: `${result.value.Etipo}`,
+//         estructura: `${result.value.Eestructura}`,
+//       };
+//       axios
+//         .put("compuesto-quimico/" + `${idCompuesto}`, peticion)
+//         .then((response) => {
+//           //console.log(response.data);
+//           if (response.status >= 200 && response.status <= 205) {
+//             //console.log(response.data[1].nombre);
+//             //console.log(response.data);
+//             window.location.reload(true);
+//             //Users.array1[key].nombreUsuario = response.data.nombreUsuario;
+//             //Users.array1[key].nombre = response.data.nombre;
+//             //Users.array1[key].apellidos = response.data.apellidos;
 
-            // swal({
-            //   icon: "success",
-            //   title: "Actualizado",
-            //   text: `
-            //     Usuario: ${result.value.EnombreUsuario}
-            //     Nombre: ${result.value.Enombre}
-            //     Apellidos: ${result.value.Eapellidos}
-            //     Email: ${result.value.Eemail}
-            //   `,
-            // });
-          }
-        })
-        .catch(function (error) {
-          if (error.status == 401) {
-            swal({
-              title: "Error acceso " + error.response.status,
-              text: "Error, no tienes acceso a esta sección.",
-              icon: "error",
-              button: "Aceptar",
-              timer: "3000",
-            });
-          } else {
-            swal({
-              title: "Error interno " + error.response.status,
-              text: "Error interno, vuelve a intentarlo en unos momentos.",
-              icon: "error",
-              button: "Aceptar",
-              timer: "3000",
-            });
-          }
-        });
-    }
-  });
-};
+//             // swal({
+//             //   icon: "success",
+//             //   title: "Actualizado",
+//             //   text: `
+//             //     Usuario: ${result.value.EnombreUsuario}
+//             //     Nombre: ${result.value.Enombre}
+//             //     Apellidos: ${result.value.Eapellidos}
+//             //     Email: ${result.value.Eemail}
+//             //   `,
+//             // });
+//           }
+//         })
+//         .catch(function (error) {
+//           if (error.status == 401) {
+//             swal({
+//               title: "Error acceso " + error.response.status,
+//               text: "Error, no tienes acceso a esta sección.",
+//               icon: "error",
+//               button: "Aceptar",
+//               timer: "3000",
+//             });
+//           } else {
+//             swal({
+//               title: "Error interno " + error.response.status,
+//               text: "Error interno, vuelve a intentarlo en unos momentos.",
+//               icon: "error",
+//               button: "Aceptar",
+//               timer: "3000",
+//             });
+//           }
+//         });
+//     }
+//   });
+// };
 
 const showHeader = true;
 
@@ -424,7 +424,7 @@ const Practices = () => {
 
       //LARAVEL CONTROLA SI EL USUARIO QUE PIDE ES ADMIN O NO
       await axios
-        .get("compuestos-quimicos")
+        .get("practicas1")
         .then((response) => {
           //console.log(response.data);
           if (response.status >= 200 && response.status <= 205) {
@@ -434,12 +434,13 @@ const Practices = () => {
               //console.log(response.data[i]);
               array.push({
                 key: i,
-                nombreCompuesto: response.data[i].nombre,
-                descripcion: response.data[i].descripcion,
-                formula: response.data[i].formula,
-                tipo: response.data[i].tipo,
-                estructura: response.data[i].estructura,
-                idCompuesto: response.data[i].id,
+                id: response.data[i].id,
+                enunciado: response.data[i].enunciado,
+                id_profesor: response.data[i].id_profesor,
+                compuestoEnMuestra: response.data[i].compuestoEnMuestra,
+                fechaInicio: response.data[i].fechaInicio,
+                fechaFin: response.data[i].fechaFin,
+                profesor: response.data[i].nombre_profesor,
               });
             }
             //console.log(array1);
